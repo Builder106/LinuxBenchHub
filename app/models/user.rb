6971abe7e23d972ed6afa3b2ea2551c71cad7545
Builder106 <vaughanolayinka@gmail.com>
2024-12-10 15:42:07 -0800

    Add Devise authentication to User model and create users migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # app/models/user.rb
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
    enum role: { viewer: 0, contributor: 1, admin: 2 }
    # Devise modules and other configurations
  end
