6c5cce7ff28f58fdf843e70a1f2b38f5aacc00c6
Builder106 <vaughanolayinka@gmail.com>
2024-12-22 08:05:08 -0800

    Add description field to performance benchmarks and update views

--- linux_bench_hub/db/schema.rb
+++ linux_bench_hub/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_21_224437) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_22_160240) do
   create_table "performance_benchmarks", force: :cascade do |t|
     t.string "name"
     t.text "data"
@@ -18,5 +18,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_21_224437) do
     t.datetime "updated_at", null: false
     t.string "linux_os"
     t.text "benchmarks"
+    t.text "description"
   end
 end
