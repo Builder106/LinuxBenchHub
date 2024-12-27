b3790871202bd8fc80c5e0053b5bacd94e8f2eaa
Builder106 <vaughanolayinka@gmail.com>
2024-12-26 20:29:27 -0800

    Remove unused navbar custom styles and clear temporary cache files; optimize asset management

---
+++ linux_bench_hub/public/assets/application-b182ffeb6d878830c2bacba43f81ce9c3c76872da89c4cc01d9a93be1643c720.js
@@ -0,0 +1,31 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// Import Bootstrap
+import "bootstrap"
+
+// Import Stimulus controllers
+import { Application } from '@hotwired/stimulus'
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+document.addEventListener('DOMContentLoaded', () => {
+   const form = document.querySelector('.needs-validation');
+   form.addEventListener('submit', (event) => {
+     const checkboxes = form.querySelectorAll('input[type="checkbox"][name="performance_benchmark[benchmarks][]"]');
+     const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+     
+     if (!isChecked) {
+       event.preventDefault();
+       event.stopPropagation();
+       alert('Please select at least one benchmark.');
+     }
+     
+     form.classList.add('was-validated');
+   }, false);
+ });
