6805359189aaf6947eae1e9736edd5dcd037f9e2
Builder106 <vaughanolayinka@gmail.com>
2024-12-26 20:29:27 -0800

    Remove unused navbar custom styles and clear temporary cache files; optimize asset management

--- linux_bench_hub/app/assets/config/manifest.js
+++ linux_bench_hub/app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascript .js
-//= link navbar_custom.css
 //= link controllers/application.js
 //= link controllers/index.js
