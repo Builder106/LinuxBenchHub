69c974aa2f82a2c3854c5c3598a220ec97794533
Builder106 <vaughanolayinka@gmail.com>
2024-12-26 19:17:28 -0800

    Fixed logout error

--- linux_bench_hub/app/models/user.rb
+++ linux_bench_hub/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
  
    has_many :performance_benchmarks, dependent: :destroy
+   has_many :notifications, dependent: :destroy
  
    # Validations
    validates :email, presence: true, uniqueness: true
